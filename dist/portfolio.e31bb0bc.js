96519100496c73ac3705c9a29cb04374da03b511
Alex Wilmer <djazium@gmail.com>
2019-11-03 16:34:17 -0500

    added page transition

--- dist/portfolio.e31bb0bc.js
+++ dist/portfolio.e31bb0bc.js
@@ -31779,13 +31779,56 @@ if ("development" === 'production') {
 },{"./cjs/react-dom.development.js":"node_modules/react-dom/cjs/react-dom.development.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _reactDom = require("react-dom");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
-(0, _reactDom.render)(_react.default.createElement("div", null, "hello, my hhhname is Enna"), document.getElementById('root'));
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; if (obj != null) { var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var Portfolio = function Portfolio() {
+  var _useState = (0, _react.useState)('home'),
+      _useState2 = _slicedToArray(_useState, 2),
+      page = _useState2[0],
+      setPage = _useState2[1];
+
+  return _react.default.createElement("div", null, _react.default.createElement("div", {
+    style: {
+      position: 'absolute',
+      opacity: page === 'home' ? 1 : 0,
+      backgroundColor: page === 'home' ? 'green' : 'rgba(0, 0, 0, 0)',
+      transition: 'all 1s ease',
+      zIndex: page === 'home' ? 1 : 0
+    }
+  }, _react.default.createElement("h1", null, "home page"), _react.default.createElement("hr", null), _react.default.createElement("button", {
+    onClick: function onClick() {
+      return setPage('cv');
+    }
+  }, "go to cv")), _react.default.createElement("div", {
+    style: {
+      position: 'absolute',
+      opacity: page === 'cv' ? 1 : 0,
+      backgroundColor: page === 'home' ? 'blue' : 'rgba(0, 0, 0, 0)',
+      transition: 'all 1s ease',
+      zIndex: page === 'cv' ? 1 : 0
+    }
+  }, _react.default.createElement("h1", null, "cv page"), _react.default.createElement("hr", null), _react.default.createElement("button", {
+    onClick: function onClick() {
+      return setPage('home');
+    }
+  }, "go to home")));
+};
+
+(0, _reactDom.render)(_react.default.createElement(Portfolio, null), document.getElementById('root'));
 },{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -31814,7 +31857,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52050" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62926" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 
+let Portfolio = () => {
+  let [page, setPage] = useState('home')
+
+
+  return (
+    <div>
+      <div style={{
+        position: 'absolute',
+        opacity: page === 'home' ? 1 : 0,
+        backgroundColor: page === 'home' ? 'green' : 'rgba(0, 0, 0, 0)',
+        transition: 'all 1s ease',
+        zIndex: page === 'home' ? 1 : 0,
+      }}>
+        <h1>home page</h1>
+        <hr />
+
+        <button onClick={() => setPage('cv')}>
+          go to cv
+        </button>
+      </div>
+      <div style={{
+        position: 'absolute',
+        opacity: page === 'cv' ? 1 : 0,
+        backgroundColor: page === 'home' ? 'blue' : 'rgba(0, 0, 0, 0)',
+        transition: 'all 1s ease',
+        zIndex: page === 'cv' ? 1 : 0,
+      }}>
+        <h1>cv page</h1>
+        <hr />
+
+        <button onClick={() => setPage('home')}>
+          go to home
+          </button>
+      </div>
+    </div>
+  )
+}
+
 render(
-  <div>hello, my hhhname is Enna</div>,
+  <Portfolio />,
   document.getElementById('root')
 )
